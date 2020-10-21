4187d6dc60ce5e1e0d56754545e31cf52b154701
catchansh99hero <70524325+catchansh99hero@users.noreply.github.com>
2020-10-21 21:42:47 +0530

    Add files via upload

---
+++ C-28/AngryBirdsStage3.5-master/Slngsht.js
@@ -0,0 +1,28 @@
+class Slngsht{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB=pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+fly(){
+
+this.sling.bodyA=nothing;
+
+
+}
+    display(){
+if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+}   
+}
